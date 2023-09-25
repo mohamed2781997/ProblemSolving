2567c0fb959b55d91e70902cc0279a4eb9c83ac2
magdy <mohammed2781997@gmail.com>
2023-09-25 18:09:02 +0300

    upload data

---
+++ src/problem_solving/Problem_Solving.java
@@ -0,0 +1,30 @@
+package problem_solving;
+
+import java.util.Scanner;
+
+public class Problem_Solving {
+
+    public static void main(String[] args) {
+        
+        //watermelon
+        Scanner s = new Scanner(System.in);
+        int num;
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println("enter your num");
+            num = s.nextInt();
+            if (num <= 2) {
+                System.out.println("no");
+            }
+            if (num % 2 == 0) {
+                System.out.println("yes");
+            } else {
+
+                System.out.println("no");
+            }
+        }
+        
+    }
+}
+        
+
